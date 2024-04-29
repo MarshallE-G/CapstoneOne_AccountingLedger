e97b726ba88f37060f55e22ff099afba8cb4cfbf
Marshall <mellisgibbs@gmail.com>
2024-04-29 18:25:49 -0400

    Created writeToFile() method and partially finished addDepositOption() method. Started makePaymentOption() method.

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,10 +1,11 @@
 package com.ps;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
     // Static global variable for transactions
@@ -20,38 +21,53 @@ public class Main {
         // Create class for storing "transactions"
 
         // Read from File first
-        inputData();
+        readFromFile();
         transactionList.displayTransactionList();
 
-        // *Home Menu*
-            // Option D: Add Deposit
-            // Option P: Make Payment (Debit)
-            // Option L: Ledger Menu
-            // Option X: Exit
+        addDepositOption();
 
-            // *Add Deposit*
-                // Ask user for deposit info
-                // Save info to transactions
-                // "...Will auto-redirect to Home menu once transaction is complete..." <-- Might not do this.
+        // Do-while loop
+            // *Home Menu*
+                // Switch statement
+                    // Option D: Add Deposit
+                    // Option P: Make Payment (Debit)
+                    // Option L: Ledger Menu
+                    // Option X: Exit
 
+                // *Add Deposit*
+                    // Ask user for deposit info
+                        // Description
+                        // Vendor
+                        // Deposit amount
+                    // Save info to transactions
+                    // "...Will auto-redirect to Home menu once transaction is complete..." <-- Might not do this.
 
-            // *Make Payment (Debit)*
-                // Ask user for payment/debit info
-                // Save info to transactions
-                // "...Will auto-redirect to Home menu once transaction is complete..." <-- Might not do this.
+                // *Make Payment (Debit)*
+                    // Ask user for payment/debit info
+                        // Description
+                        // Vendor
+                        // Payment/debit amount
+                    // Save info to transactions
+                    // "...Will auto-redirect to Home menu once transaction is complete..." <-- Might not do this.
 
-            // **Ledger Menu**
-                // Option A:
-                // Option D:
-                // Option P:
-                // Option R: ***Report Menu***
-                    // Option 1) Month To Date
-                    // Option 2) Previous Month
-                    // Option 3) Year To Date
-                    // Option 4) Previous Year
-                    // Option 5) Search by Vendor
-                    // Option 0) Back
-                // Option H: Home
+            // Do-while loop
+                // **Ledger Menu**
+                    // Switch statement
+                        // Option A: All
+                        // Option D: Deposits
+                        // Option P: Payments
+                    // Do-while loop
+                        // Option R: ***Report Menu***
+                            // Switch statement
+                                // Option 1) Month To Date
+                                // Option 2) Previous Month
+                                // Option 3) Year To Date
+                                // Option 4) Previous Year
+                                // Option 5) Search by Vendor
+                                // Option 0) Back
+                    // End of Report's Do-while loop
+                        // Option H: Home
+            // End of Ledger Menu Do-while loop
 
 
 
@@ -59,7 +75,7 @@ public class Main {
     }
 
     // InputData method to read from File
-    public static void inputData() {
+    public static void readFromFile() {
         try {
             BufferedReader bufReader = new BufferedReader(new FileReader("transactions.txt"));
 
@@ -83,7 +99,68 @@ public class Main {
     }
 
     // OutputData method to write to File
+    public static void writeToFile(Transaction transaction) {
+        // Format Date and Time
+
+        try {
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("transactions.txt"));
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            String formattedDate = transaction.getDate().format(dateFormatter);
+            String formattedTime = transaction.getTime().format(timeFormatter);
+
+            bufWriter.write(
+                    formattedDate + "|" +
+                    formattedTime + "|" +
+                    String.format("%s|%s|%.2f\n",
+                    transaction.getDescription(),
+                    transaction.getVendor(),
+                    transaction.getAmount()
+                    ));
+
+            System.out.printf("%s|%s|%s|%s|%.2f\n",
+                    formattedDate,
+                    formattedTime,
+                    transaction.getDescription(),
+                    transaction.getVendor(),
+                    transaction.getAmount());
+
+            System.out.println("Printed Successfully......");
+
+            bufWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     // Static methods for Menus
+    public static void addDepositOption() {
+        String description;
+        String vendor;
+        float amount;
+
+        Scanner scanner = new Scanner(System.in);
+        // *Add Deposit*
+            // Ask user for deposit info
+                // Description
+        System.out.println("Enter a description of the deposit (e.g. \"Invoice 1001 paid\"): ");
+        description = scanner.nextLine();
+                // Vendor
+        System.out.println("Enter name of vendor you're depositing to (e.g. Joe): ");
+        vendor = scanner.nextLine();
+                // Deposit amount
+        System.out.println("Enter deposit amount: ");
+        amount = scanner.nextFloat();
+        System.out.println();
+            // Save info to transactions
+        Transaction transaction = new Transaction(LocalDate.now(), LocalTime.now(), description, vendor, amount);
+            // Add transaction to transactions file
+        writeToFile(transaction);
+            // "...Will auto-redirect to Home menu once transaction is complete..." <-- Might not do this.
+    }
+
+    public static void makePaymentOption() {
+
+    }
 
 }
